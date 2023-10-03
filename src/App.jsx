851297860cbb7439e036797bd53f059c389e5914
nahum_8 <nahumpl@MacBook-Pro-de-Nahum.local>
2023-10-03 12:29:00 +0200

    proyecto react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState({r : 255, g : 255, b : 255})
+
+  useEffect(() => {
+    fetch("https://prueba-backend-7d7m.onrender.com/")
+    .then(respuesta => respuesta.json())
+    .then(color => setColor(color))
+  })
+
+  return (
+    <>
+      <section style={{backgroundColor : `rgb(${[color.r, color.g,color.b].join(",")})`}}>
+        <h1>{ [color.r, color.g,color.b].join(",") }</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
